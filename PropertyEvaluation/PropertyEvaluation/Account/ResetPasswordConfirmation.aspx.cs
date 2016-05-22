747e9a86dbd34e1c618738e872ee90e0300b92fc
MatthewBrand <mbrand@gmail.com>
2016-05-22 09:12:05 -0500

    Initial project creation

---
+++ PropertyEvaluation/PropertyEvaluation/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace PropertyEvaluation.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ PropertyEvaluation/PropertyEvaluation/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PropertyEvaluation.Startup))]
+namespace PropertyEvaluation
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
